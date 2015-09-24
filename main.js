a379c19718a4d552838d6e3709f0a71f303fbb3a
Jose Plata <jose.a.plata@gmail.com>
2015-09-24 10:33:46 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,35 @@
+// Data types:
+
+console.log("Jose"); // String
+console.log(26); // Numbers
+console.log(10>9); // Boolean -> Returns true
+
+var x;
+console.log(x); // Undefined
+
+// Variables
+
+var myAge = "26"; //Variables have keys and values. My age is the key, 26 the value
+console.log(myAge);
+
+// Arrays 
+
+books = ["Moby Dick", "Dracula", "On The Road"];
+console.log(books);
+
+// Logic
+
+console.log(2!=3);
+console.log(2===3);
+console.log(2=='2');
+console.log(2>3);
+console.log(2<3);
+
+// Functions
+
+function sayHi(myGreeting){
+	console.log(myGreeting);
+}
+
+sayHi('Hello there!');
+
